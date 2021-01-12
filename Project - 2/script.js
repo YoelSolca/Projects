721c218b499531c8677acd7820dd6755d7ff217a
Yoel <63026075+YoelSolca@users.noreply.github.com>
2021-01-12 18:20:43 -0300

    Version - 1

---
+++ Project - 2/script.js
@@ -0,0 +1,7 @@
+function changeColor(event){
+  document.querySelectorAll('span.active').forEach(function(item){
+    item.classList.remove('active')
+  })
+
+  event.target.classList.add('active')
+}
